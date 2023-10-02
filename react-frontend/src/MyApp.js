d029a2b1228271ad2648c8078a9dba364e686d86
spruhaN <spruhanayak2004@gmail.com>
2023-10-01 21:19:46 -0700

    added forms to add characters

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,27 +1,10 @@
 // src/MyApp.js
 import React, {useState} from 'react';
 import Table from "./Table";
-
+import Form from './Form';
 
   function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-          name: "Charlie",
-          job: "Janitor",
-        },
-        {
-          name: "Mac",
-          job: "Bouncer",
-        },
-        {
-          name: "Dee",
-          job: "Aspring actress",
-        },
-        {
-          name: "Dennis",
-          job: "Bartender",
-        },
-      ]); 
+    const [characters, setCharacters] = useState([]);
 
       function removeOneCharacter (index) {
           const updated = characters.filter((character, i) => {
@@ -29,11 +12,15 @@ import Table from "./Table";
           });
         setCharacters(updated);
       }
+      function updateList(person) {
+        setCharacters([...characters, person]);
+      }
       return (
         <div className="container">
-            <Table characterData={characters} 
-              removeCharacter={removeOneCharacter} />
-        </div>  
+          <Table characterData={characters} 
+            removeCharacter={removeOneCharacter} />
+          <Form handleSubmit={updateList} />
+        </div>
       )
   }
 
