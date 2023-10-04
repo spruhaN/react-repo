7c36e268686f1c5c0c3e4ef3aef76b85e6671224
spruhaN <spruhanayak2004@gmail.com>
2023-10-04 16:27:35 -0700

    adding isers by name

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -36,13 +36,30 @@ const users = {
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello LMAO World!');
 });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });   
 
+// app.get('/users', (req, res) => {
+//     res.send(users);
+// });
+
+const findUserByName = (name) => { 
+    return users['users_list']
+        .filter( (user) => user['name'] === name); 
+}
+
 app.get('/users', (req, res) => {
-    res.send(users);
+    const name = req.query.name;
+    if (name != undefined){
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
 });
